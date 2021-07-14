b6bbd8a0a575e1747246d3d8e65c5ffc66730cac
BBLF97 <87401029+BBLF97@users.noreply.github.com>
2021-07-14 10:23:34 +0900

    Add files via upload

---
+++ pf-02/script/script.js
@@ -0,0 +1,8 @@
+$(function(){
+        $(".accordion-menu>ul>li").hover(function(){
+            $(this).find(".submenu").stop().slideDown(500);
+        },
+        function(){
+            $(this).find(".submenu").stop().slideUp(500);
+        });     
+    });
